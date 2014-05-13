6c9e8fcf99d39c9949bff0d643fde5b2a1413475
Julien Portalier <julien@portalier.com>
2014-05-13 11:24:39 +0200

    Use #sinapse_publish instead of #publish to avoid clashes with other libraries.
    The #publish is now an alias for #sinapse_publish and won't be
    defined if a #publish already exists.

--- lib/sinapse/publishable.rb
+++ lib/sinapse/publishable.rb
@@ -2,7 +2,11 @@ require 'active_support/core_ext/string/inflections'
 
 module Sinapse
   module Publishable
-    def publish(message)
+    def self.included(klass)
+      klass.alias_method :publish, :sinapse_publish unless klass.respond_to?(:publish)
+    end
+
+    def sinapse_publish(message)
       Sinapse.redis { |redis| redis.publish(sinapse_channel, message) }
     end
 
